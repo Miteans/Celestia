3e196117bb141f68ceeb20bc3926cb1b0b0ca03a
Pallavi2000 <hmpsharma@gmail.com>
2020-08-27 13:48:57 +0530

    Completion of Left Nav bar of menu

--- MusiCafe/UI Side/src/app/header/header.component.ts
+++ MusiCafe/UI Side/src/app/header/header.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  hide = true;
+  show_button = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  show_hidden(){
+    if(this.hide){
+      this.hide = false
+    }
+    else{
+      this.hide = true
+    }
+  }
+
+  call_button(){
+    this.show_button = true;
+  }
 }
